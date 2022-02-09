435613bba318fcfa46b23ac8d656684d60ecbdc0
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2022-02-09 11:55:45 +0100

    loop works

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+let countNumber = 0;
+const array = [];
+
+window.addEventListener("load", initArrayLoop);
+
+function initArrayLoop() {
+  if (countNumber < 22) {
+    setTimeout(arrayLoop, 1000);
+  }
+}
+
+function arrayLoop() {
+  if (countNumber < 9) {
+    array.unshift(countNumber);
+    console.log(array);
+    countNumber++;
+  } else {
+    array.unshift(countNumber);
+    array.length = 9;
+    console.log(array);
+    countNumber++;
+  }
+  initArrayLoop();
+}
